b8dc0cd0e3d366f624461b1ad1215cb0d64e7823
adventure2023 <125820021+adventure2023@users.noreply.github.com>
2023-09-25 21:53:12 -0700

    added mp4

--- ytmp3.py
+++ ytmp3.py
@@ -5,6 +5,8 @@ from pytube import YouTube
 st.title("Get MP3") 
 st.subheader("Enter the URL:") 
 url = st.text_input(label='URL')
+# Create a checkbox
+get_mp4 = st.checkbox("Get MP4")
 destination = '.'
 input_done = st.button("Enter")
 
@@ -17,6 +19,7 @@ if input_done and  url != '':
     '''.format(yt.title , yt.length ))
     video = yt.streams
     if len(video) > 0:
+        file_ext = ".mp3"
         downloaded , download_audio = False , False
         # download_video = st.button("Download Video")
         # if yt.streams.filter(only_audio=True):
@@ -24,13 +27,18 @@ if input_done and  url != '':
         # if download_video:            
         #     outfile = video.get_highest_resolution().download(output_path=destination)
         #     downloaded = True
-        if yt.streams.filter(only_audio=True):# download_audio:
+        if get_mp4:
+            only_audio = False
+            outfile = video.get_highest_resolution().download(output_path=destination)
+            downloaded = True
+            file_ext = ".mp4"
+        elif yt.streams.filter(only_audio=True):# download_audio:
             outfile = video.filter(only_audio=True).last().download(output_path=destination)
             downloaded = True
         if downloaded:
             # save the file
             base, ext = os.path.splitext(outfile)
-            new_file = base + '.mp3'
+            new_file = base + file_ext
             os.rename(outfile, new_file)
             with open(new_file, "rb") as f:
                 st.download_button('Download file', f, file_name=os.path.basename(new_file))
